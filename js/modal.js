bed8c00805f309d31c5a81c43106cfef369842d4
DrkSephy <DrkSephy1025@gmail.com>
2019-01-06 20:40:36 -0500

    Created experimental modal

---
+++ js/modal.js
@@ -0,0 +1,36 @@
+function handleClick(e) {
+	const targetId = e.target.id;
+	const targetTag = e.target.tagName;
+	const targetAlt = e.target.alt;
+
+	const modal = document.getElementById('myModal');
+	const isModalVisible = modal.style.display === 'flex' ? true : false;
+
+	if (targetTag === 'IMG' && !isModalVisible) {
+		const imgElement = document.getElementById(targetId);
+		const imgSrc = imgElement.src;
+		const modalImg = document.getElementsByClassName('modal-content')[0];
+
+		// Display the modal
+		modal.style.display = 'flex';
+
+		// Change the source
+		modalImg.src = imgSrc;
+
+		// When the modal is active, we prevent scrolling
+		document.body.style.overflow = 'hidden';
+
+		const span = document.getElementsByClassName('close')[0];
+		const captionText = document.getElementsByClassName('caption')[0];
+
+		captionText.innerHTML = targetAlt;
+
+		span.onclick = function() { 
+		  modal.style.display = 'none';
+		}
+	} else {
+		// Hide modal, reset styles
+		modal.style.display = 'none';
+		document.body.style.overflow = 'auto';
+	}
+}
